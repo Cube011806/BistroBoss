705cdc425190f629100c60af5ee06e2fa3072a5d
Klaudiusz Kozieł <klaudiuszkoziel@o2.pl>
2025-04-08 10:30:15 +0200

    BISTROBOSS-6 Dodanie atrybutów klasy Uzytkownik

--- BistroBoss/Models/Uzytkownik.cs
+++ BistroBoss/Models/Uzytkownik.cs
@@ -5,5 +5,9 @@ namespace BistroBoss.Models
     public class Uzytkownik : IdentityUser
     {
         public int AccessLevel { get; set; }
+        public string? Imie { get; set; }
+        public string? Nazwisko { get; set; }
+        public DateTime? DataUrodzenia { get; set; }
+
     }
 }
