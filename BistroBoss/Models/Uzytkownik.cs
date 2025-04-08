ed5de970fbb31a06860dbb9ee67a557cb9c7643a
Klaudiusz Kozieł <klaudiuszkoziel@o2.pl>
2025-04-08 10:38:57 +0200

    BISTROBOSS-6 Dodano relacje do klasy Użytkownik

--- BistroBoss/Models/Uzytkownik.cs
+++ BistroBoss/Models/Uzytkownik.cs
@@ -4,10 +4,12 @@ namespace BistroBoss.Models
 {
     public class Uzytkownik : IdentityUser
     {
-        public int AccessLevel { get; set; }
+        public int AccessLevel { get; set; } = 0; // 0 - Klient, 1 - Pracownik, 2 - Administrator
         public string? Imie { get; set; }
         public string? Nazwisko { get; set; }
         public DateTime? DataUrodzenia { get; set; }
+        public virtual ICollection<Opinia> Opinie { get; set; } = new List<Opinia>();
+        public virtual ICollection<Zamowienie> Zamowienia { get; set; } = new List<Zamowienie>();
 
     }
 }
