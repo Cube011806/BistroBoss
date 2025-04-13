7f14b6906558c8c6d35d0587c8b087abcac2d618
Klaudiusz Kozieł <klaudiuszkoziel@o2.pl>
2025-04-13 15:24:02 +0200

    BISTROBOSS-15, BISTROBOSS-16, BISTROBOSS-17

---
+++ BistroBoss/Controllers/MenuController.cs
@@ -0,0 +1,31 @@
+﻿using BistroBoss.Data;
+using BistroBoss.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BistroBoss.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly UserManager<Uzytkownik> _userManager;
+
+        public MenuController(ApplicationDbContext dbContext, UserManager<Uzytkownik> userManager)
+        {
+            _dbContext = dbContext;
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            var kategorie = _dbContext.Kategorie.Include(k=>k.Produkty).ToList();
+            return View(kategorie);
+        }
+        public IActionResult Details(int id)
+        {
+            var produkty = _dbContext.Produkty.Include(p => p.Kategoria);
+            var produkt = produkty.FirstOrDefault(p => p.Id == id);
+            return View(produkt); 
+        }
+    }
+}
